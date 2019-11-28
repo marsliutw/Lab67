b44a2653f0b1424801c7a27c0c8d0bef5281a163
Jason Clay <mars312041@gmail.com>
2019-11-28 20:15:36 +0800

    first commit

---
+++ Lab6kt/app/src/main/java/com/example/lab6kt/trans_list.kt
@@ -0,0 +1,12 @@
+package com.example.lab6kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class trans_list : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.trans_list)
+    }
+}
